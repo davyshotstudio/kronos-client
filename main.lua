efa8e558c6065f042613996ceb7f26fe3f75b672
Wilbert Lam <lwilbert@uber.com>
2020-12-13 19:25:09 -0800

    initial commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- This is the entry point for all code execution in Solar2D
+--
+-----------------------------------------------------------------------------------------
+
+-- Hide the default OS status bar on mobile devices
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Include the Corona "composer" module
+local composer = require "composer"
+
+-- Load menu screen
+composer.gotoScene("scenes.menu.menu")
