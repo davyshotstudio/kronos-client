f208f8dcabea8767cb09232a43a7769711a9b1c4
Wilbert Lam <lwilbert@uber.com>
2021-01-31 11:01:27 -0800

    Add popup on hold

---
+++ scenes/game/batter-card-zoomed-overlay.lua
@@ -0,0 +1,97 @@
+-- Public imports
+local composer = require("composer")
+local widget = require("widget")
+
+-- Local imports
+local assetUtil = require("scenes.game.utilities.asset-util")
+local constants = require("scenes.game.utilities.constants")
+
+-- Scene setup
+local scene = composer.newScene()
+local SCENE_NAME = constants.SCENE_NAME_BATTER_CARD_ZOOMED_OVERLAY
+local sceneGroup
+
+-- Services
+local viewManager
+local batterManager
+
+local onBackgroundClick
+
+function scene:create(event)
+  -- Retrieve DI instances of the managers
+  viewManager = composer.getVariable("viewManager")
+  batterManager = composer.getVariable("batterManager")
+
+  -- Register scene domain into the ViewManager
+  viewManager:registerScene(SCENE_NAME)
+end
+
+function scene:show(event)
+  sceneGroup = self.view
+  local phase = event.phase
+  local parent = event.parent -- Reference to the parent scene object
+
+  local card = event.params["card"]
+
+  if (phase == "did") then
+    -- Fake tin that covers the existing screen
+    local backgroundTintOverlay =
+      viewManager:addComponent(
+      SCENE_NAME,
+      "BACKGROUND_TINT_OVERLAY",
+      (function()
+        local backgroundTint =
+          widget.newButton(
+          {
+            shape = "rect",
+            width = display.actualContentWidth,
+            height = display.actualContentHeight,
+            left = 0,
+            top = 0,
+            onRelease = onBackgroundClick
+          }
+        )
+        backgroundTint:setFillColor(0.1)
+        backgroundTint.alpha = 0.75
+        sceneGroup:insert(backgroundTint)
+        return backgroundTint
+      end)()
+    )
+
+    local actionCardView =
+      viewManager:addComponent(
+      SCENE_NAME,
+      "ACTION_CARD_" .. card:getID(),
+      (function()
+        local cardButton =
+          widget.newButton {
+          font = "asul.ttf",
+          defaultFile = assetUtil.resolveAssetPath(card:getBattingAction():getPictureURL())
+        }
+        cardButton.x = display.contentCenterX
+        cardButton.y = display.contentCenterY
+        sceneGroup:insert(cardButton)
+        return cardButton
+      end)()
+    )
+  end
+end
+
+function scene:hide(event)
+  local phase = event.phase
+
+  if (phase == "did") then
+    viewManager:removeComponents(SCENE_NAME)
+  end
+end
+
+scene:addEventListener("hide", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("create", scene)
+
+-- Exit from overlay
+function onBackgroundClick()
+  composer.hideOverlay("fade", 200)
+end
+
+return scene
